1a8b22ea258c726be7f4c78d568fba9977de8062
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-06-02 01:57:49 -0300

    Feature: should not be able to create a new user with a repeated email

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -9,25 +9,21 @@ class UserRepository implements IUsersRepository {
   private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
-    const users = this.users.find(user => user.id === id);
+    const findUser = this.users.find(user => user.id === id);
 
-    return users;
+    return findUser;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const users = this.users.find(user => user.email === email);
+    const findUser = this.users.find(user => user.email === email);
 
-    return users;
+    return findUser;
   }
 
-  public async create({
-    name,
-    email,
-    password,
-  }: ICreateUserDTO): Promise<User> {
+  public async create(userData: ICreateUserDTO): Promise<User> {
     const user = new User();
 
-    Object.assign(user, { id: uuid() }, name, email, password);
+    Object.assign(user, { id: uuid() }, userData);
 
     this.users.push(user);
 
--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
@@ -13,5 +14,24 @@ describe('CreateUser', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with a repeated email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
