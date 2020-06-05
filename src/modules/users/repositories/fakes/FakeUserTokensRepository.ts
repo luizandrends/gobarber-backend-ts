a84bbfcf2657df03d1e332c50888612050fc6f8b
Luiz Andre <luizxd234@gmail.com>
2020-06-05 20:45:45 -0300

    Chore: creating FakeUserTokenRepository

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,24 @@
+import { uuid } from 'uuidv4';
+
+import UserToken from '../../infra/typeorm/entities/UserToken';
+import IUsersTokenRepository from '../IUserTokensRepository';
+
+class FakeUserTokensRepository implements IUsersTokenRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export default FakeUserTokensRepository;
