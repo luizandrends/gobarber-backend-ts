953f83aea1b3456ff5f53ecb39600e1f458aea7e
Luiz Andre <luizxd234@gmail.com>
2020-06-09 09:41:56 -0300

    Chore: Passing in the test

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
@@ -17,7 +18,23 @@ class SendForgotPasswordEmailService {
     private userTokensRepository: IUserTokensRepository
   ) {}
 
-  public async execute({ password, token }: IRequest): Promise<void> {}
+  public async execute({ password, token }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exist');
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    user.password = password;
+
+    await this.usersRepository.save(user);
+  }
 }
 
 export default SendForgotPasswordEmailService;
